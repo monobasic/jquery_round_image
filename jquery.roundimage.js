a74c6a59bb4b919bd30d6ec3ee0e1b085f5a5681
André Abt <info@monobasic.com>
2014-04-10 17:52:46 +0200

    converted to proper jQuery plugin and added uniqueId for clip paths

--- jquery.roundimage.js
+++ jquery.roundimage.js
@@ -1,9 +1,15 @@
-/* jQuery Rounded Image by www.andre-abt.com */
-$.fn.roundImage = function() {
-    var element = $(this),
-        svgWidth = element.width(),
-        svgHeight = element.height(),
-        imgUrl = element.attr('src');
-
-        element.replaceWith('<svg width="' + svgWidth + '" height="' + svgHeight + '"><clipPath id="clipCircle"><circle r="50" cx="50" cy="50"/></clipPath><!-- the following separated src attribute acts as a browser fallback --><image clip-path="url(#clipCircle)" xlink:href="' + imgUrl + '" src="' + imgUrl + '" width="' + svgWidth + '" height="' + svgHeight + '"></image></svg>');
-};
+(function ($) {
+    $.fn.roundImage = function() {
+        return this.each(function() {
+            var element = $(this),
+                svgWidth = element.width(),
+                svgHeight = element.height(),
+                imgUrl = element.attr('src'),
+                uniqId = function() {
+                    return Math.round(new Date().getTime() + (Math.random() * 100));
+                },
+                svgClipPathId = uniqId();
+                element.replaceWith('<svg width="' + svgWidth + '" height="' + svgHeight + '"><clipPath id="' + svgClipPathId + '"><circle r="' + svgWidth/2 + '" cx="' + svgWidth/2 + '" cy="' + svgHeight/2 + '"/></clipPath><!-- the following separated src attribute acts as a browser fallback --><image clip-path="url(#' + svgClipPathId + ')" xlink:href="' + imgUrl + '" src="' + imgUrl + '" width="' + svgWidth + '" height="' + svgHeight + '"></image></svg>');
+        });
+    };
+}(jQuery));
