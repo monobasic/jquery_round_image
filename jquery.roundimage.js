6ba36e3b11fb4676142e71778a47d372518e5f27
André Abt <info@monobasic.com>
2014-04-11 08:39:51 +0200

    removed svg comment

--- jquery.roundimage.js
+++ jquery.roundimage.js
@@ -9,7 +9,7 @@
                     return Math.round(new Date().getTime() + (Math.random() * 100));
                 },
                 svgClipPathId = uniqId();
-                image.replaceWith('<svg width="' + svgWidth + '" height="' + svgHeight + '"><clipPath id="' + svgClipPathId + '"><circle r="' + svgWidth/2 + '" cx="' + svgWidth/2 + '" cy="' + svgHeight/2 + '"/></clipPath><!-- the following separated src attribute acts as a browser fallback --><image clip-path="url(#' + svgClipPathId + ')" xlink:href="' + imgSrc + '" src="' + imgSrc + '" width="' + svgWidth + '" height="' + svgHeight + '"></image></svg>');
+                image.replaceWith('<svg width="' + svgWidth + '" height="' + svgHeight + '"><clipPath id="' + svgClipPathId + '"><circle r="' + svgWidth/2 + '" cx="' + svgWidth/2 + '" cy="' + svgHeight/2 + '"/></clipPath><image clip-path="url(#' + svgClipPathId + ')" xlink:href="' + imgSrc + '" src="' + imgSrc + '" width="' + svgWidth + '" height="' + svgHeight + '"></image></svg>');
         });
     };
 }(jQuery));
