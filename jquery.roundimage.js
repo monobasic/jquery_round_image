457530fe28c11b4faad4d472d6e07acaee4e3224
André Abt <info@monobasic.com>
2014-04-11 08:35:24 +0200

    renamed some stuff

--- jquery.roundimage.js
+++ jquery.roundimage.js
@@ -1,15 +1,15 @@
 (function ($) {
     $.fn.roundImage = function() {
         return this.each(function() {
-            var element = $(this),
-                svgWidth = element.width(),
-                svgHeight = element.height(),
-                imgUrl = element.attr('src'),
+            var image = $(this),
+                svgWidth = image.width(),
+                svgHeight = image.height(),
+                imgSrc = image.attr('src'),
                 uniqId = function() {
                     return Math.round(new Date().getTime() + (Math.random() * 100));
                 },
                 svgClipPathId = uniqId();
-                element.replaceWith('<svg width="' + svgWidth + '" height="' + svgHeight + '"><clipPath id="' + svgClipPathId + '"><circle r="' + svgWidth/2 + '" cx="' + svgWidth/2 + '" cy="' + svgHeight/2 + '"/></clipPath><!-- the following separated src attribute acts as a browser fallback --><image clip-path="url(#' + svgClipPathId + ')" xlink:href="' + imgUrl + '" src="' + imgUrl + '" width="' + svgWidth + '" height="' + svgHeight + '"></image></svg>');
+                image.replaceWith('<svg width="' + svgWidth + '" height="' + svgHeight + '"><clipPath id="' + svgClipPathId + '"><circle r="' + svgWidth/2 + '" cx="' + svgWidth/2 + '" cy="' + svgHeight/2 + '"/></clipPath><!-- the following separated src attribute acts as a browser fallback --><image clip-path="url(#' + svgClipPathId + ')" xlink:href="' + imgSrc + '" src="' + imgSrc + '" width="' + svgWidth + '" height="' + svgHeight + '"></image></svg>');
         });
     };
 }(jQuery));
